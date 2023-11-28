f440fcc7fb37a3f8b47fe48a719cbcf5298c2f55
Zolo-Ryan <utkarshswaroopsri187@gmail.com>
2023-11-29 01:59:12 +0530

    moved google api code to a seperate file

--- routes/price-page.js
+++ routes/price-page.js
@@ -3,10 +3,10 @@ const User = require("../models/user");
 const Confirm = require("../models/confirm-user");
 const puppeteer = require("puppeteer");
 const hbs = require("handlebars");
-require("dotenv").config();
 const router = require("express").Router();
 const {upload} = require("../utils/multerUtil");
 const multer = require("multer");
+const uploadFile = require("../utils/fileUploadingOnGoogleDrive");
 const storage = multer.diskStorage({
     destination: (req,file,cb) => {
         cb(null,"./uploads/paymentSS");
@@ -17,22 +17,6 @@ const storage = multer.diskStorage({
 });
 const upload2 = multer({storage});
 
-const { google } = require("googleapis");
-
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URL = process.env.REDIRECT_URL;
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-const oauth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URL
-);
-oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-const drive = google.drive({
-  version: "v3",
-  auth: oauth2Client,
-});
 
 const path = require("path");
 //function to compile the template html into the pdf by giving it the data
@@ -103,7 +87,7 @@ router
     const leader = await User.findOne({ _id: leaderID });
     const array = await User.find({ leaderID });
     array.push(leader);
-    
+    const links = [];
     (async function(){
       try {
         array.forEach( async (e) => {
@@ -124,8 +108,9 @@ router
             path: pathF,
             format: "A3",
             printBackground: true
-          });
-          await uploadFile(pathF,leader,e);
+          })
+          const link = await uploadFile.uploadFile(pathF,leader,e);
+          console.log(link)
           await browser.close();
         });
 
@@ -142,20 +127,5 @@ function stringToPrice(string) {
   if (string === "STARNIGHT") return 550;
   if (string === "STAY") return 150;
   return 0;
-}
-async function uploadFile(filePath, leader, e) {
-  try {
-    const res = await drive.files.create({
-      requestBody: {
-        name: `${leader.teamName}-${e.username}`,
-        mimeType: "application/pdf",
-      },
-      media: {
-        mimeType: "application/pdf",
-        body: fs.createReadStream(filePath),
-      },
-    });
-    console.log(res.data);
-  } catch (error) {}
 }
 module.exports = router;
---
+++ utils/fileUploadingOnGoogleDrive.js
@@ -0,0 +1,59 @@
+const fs = require("fs-extra");
+const { google } = require("googleapis");
+require("dotenv").config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URL = process.env.REDIRECT_URL;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL
+);
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+const drive = google.drive({
+  version: "v3",
+  auth: oauth2Client,
+});
+
+async function uploadFile(filePath, leader, e) {
+  try {
+    const res = await drive.files.create({
+      requestBody: {
+        name: `${leader.teamName}-${e.username}`,
+        mimeType: "application/pdf",
+      },
+      media: {
+        mimeType: "application/pdf",
+        body: fs.createReadStream(filePath),
+      },
+    });
+    console.log(res.data);
+    const url = await publicURL(res.data.id);
+    return url;
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function publicURL(fileId) {
+  try {
+    await drive.permissions.create({
+      fileId,
+      requestBody: {
+        role: "reader",
+        type: "anyone",
+      },
+    });
+    const result = await drive.files.get({
+      fileId,
+      fields: "webViewLink, webContentLink",
+    });
+    console.log(result.data);
+    return result.data.webViewLink;
+  } catch (error) {}
+}
+
+module.exports = {
+    uploadFile,publicURL
+};

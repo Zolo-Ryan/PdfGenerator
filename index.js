212509f08f6c7be5b476fdcd689eb895ed48e760
Zolo-Ryan <utkarshswaroopsri187@gmail.com>
2023-12-10 01:58:42 +0530

    Removed unnecessary ids and in future will remove the unecessary data model

--- index.js
+++ index.js
@@ -16,12 +16,9 @@ app.set("view engine","ejs");
 app.set("views",path.resolve("./views"));
 app.use("/price-page",priceRouter);
 app.use("/form",formRouter);
-app.use("/",(req,res) => {
-    return res.render("404Page",{type: false});
-});
 
 app.get("*",(req,res,next) => {
-    return res.status(404).render("404Page",{type: true});
+    return res.status(404).render("404Page");
 })
 
 app.listen(PORT, () => {
